19b809ee640a26e7b4130294ca0c3bfb553bfff0
John Reese <john@noswap.com>
2020-05-11 19:18:44 -0700

    Simplify outputs, include elapsed/remaining times

--- StatusWatcher.py
+++ StatusWatcher.py
@@ -1,6 +1,7 @@
 import sys
+import textwrap
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, Union
 
 from UM.Application import Application
 from UM.Logger import Logger
@@ -10,11 +11,28 @@ from UM.Qt.Duration import Duration
 
 from cura.CuraApplication import CuraApplication
 from cura.UI.PrintInformation import PrintInformation
+from cura.Settings.MachineManager import MachineManager
 from PyQt5.QtCore import QTimer
 
 
-def duration_text(duration: Duration) -> str:
-    if duration.valid:
+def duration_text(duration: Union[Duration, int]) -> str:
+    if isinstance(duration, int):
+        days, hours, minutes, = 0, 0, 0
+        seconds = duration
+
+        while seconds > 86400:
+            days += 1
+            seconds -= 86400
+
+        while hours > 3600:
+            hours += 1
+            seconds -= 3600
+
+        while minutes > 60:
+            minutes += 1
+            seconds -= 60
+
+    elif isinstance(duration, Duration) and duration.valid:
         days, hours, minutes, seconds = (
             duration.days,
             duration.hours,
@@ -22,20 +40,23 @@ def duration_text(duration: Duration) -> str:
             duration.seconds,
         )
 
-        if days <= 0 and hours <= 0 and minutes <= 0 and seconds <= 0:
-            return ""
+    else:
+        return ""
 
-        if days > 0:
-            tpl = "{days}:{hours:02}:{minutes:02}:{seconds:02}"
-        else:
-            tpl = "{hours}:{minutes:02}:{seconds:02}"
+    if days <= 0 and hours <= 0 and minutes <= 0 and seconds <= 0:
+        return ""
 
-        return tpl.format(days=days, hours=hours, minutes=minutes, seconds=seconds,)
+    if days > 0:
+        tpl = "{days}:{hours:02}:{minutes:02}:{seconds:02}"
+    else:
+        tpl = "{hours}:{minutes:02}:{seconds:02}"
+
+    return tpl.format(days=days, hours=hours, minutes=minutes, seconds=seconds,)
 
     return ""
 
 
-def print_text(lines: Iterable[str], path: Path):
+def write_lines(lines: Iterable[str], path: Path):
     content = "\n".join(lines) + "\n"
     path.write_text(content)
 
@@ -45,11 +66,8 @@ class StatusWatcher(Extension):
         super().__init__()
 
         self._app = application
-        self._short = Path(
-            "~/cura-print-short.txt"
-        ).expanduser()  # should be configurable 🤷‍♀️
-        self._long = Path(
-            "~/cura-print-long.txt"
+        self._path = Path(
+            "~/cura-print-status.txt"
         ).expanduser()  # should be configurable 🤷‍♀️
 
         self._timer = QTimer()
@@ -58,25 +76,37 @@ class StatusWatcher(Extension):
 
     def dump_status(self):
         try:
-            short = []
-            long = []
+            job_name = ""
+            total_time = ""
+            elapsed_time = ""
+            remaining_time = ""
 
             info = self._app.getPrintInformation()
             if info is not None:
-                short.append("Job Name: {}".format(info.jobName))
+                job_name = info.jobName
+                total_time = duration_text(info.currentPrintTime)
 
-                duration = duration_text(info.currentPrintTime)
-                short.append("Print Time: {}".format(duration))
+            devices = self._app.getMachineManager().printerOutputDevices
+            if devices:
+                printer = printers[0].activePrinter
+                if printer is not None:
+                    job = printer.activePrintJob
+                    if job is not None:
+                        elapsed_time = duration_text(job.timeElapsed)
+                        remaining_time = duration_text(job.timeRemaining)
 
-            print_text(short, self._short)
-
-            if info is not None:
-                for key, value in sorted(info.getFeaturePrintTimes().items()):
-                    if value.valid:
-                        duration = duration_text(value)
-                        long.append("  {}: {}".format(key, duration))
-
-            print_text(short + long, self._long)
+            content = """
+                Job Name: {job_name}
+                Total Time: {total_time}
+                Elapsed: {elapsed_time}
+                Remaining: {remaining_time}
+            """.format(
+                job_name=job_name,
+                total_time=total_time,
+                elapsed_time=elapsed_time,
+                remaining_time=remaining_time,
+            )
+            self._path.write_text(textwrap.dedent(content))
 
         except Exception as e:
             Logger.log("w", "failed to write status file: {}".format(e))
