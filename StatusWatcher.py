50e5492151b19c133a88cccb8a44a721d9a0d52d
John Reese <john@noswap.com>
2020-05-11 20:01:04 -0700

    Fix time formatting

--- StatusWatcher.py
+++ StatusWatcher.py
@@ -1,17 +1,14 @@
 import sys
 import textwrap
+import unittest
 from pathlib import Path
 from typing import Iterable, Union
 
-from UM.Application import Application
 from UM.Logger import Logger
-from UM.PluginRegistry import PluginRegistry
 from UM.Extension import Extension
 from UM.Qt.Duration import Duration
 
 from cura.CuraApplication import CuraApplication
-from cura.UI.PrintInformation import PrintInformation
-from cura.Settings.MachineManager import MachineManager
 from PyQt5.QtCore import QTimer
 
 
@@ -24,11 +21,11 @@ def duration_text(duration: Union[Duration, int]) -> str:
             days += 1
             seconds -= 86400
 
-        while hours > 3600:
+        while seconds > 3600:
             hours += 1
             seconds -= 3600
 
-        while minutes > 60:
+        while seconds > 60:
             minutes += 1
             seconds -= 60
 
@@ -54,11 +51,6 @@ def duration_text(duration: Union[Duration, int]) -> str:
     return tpl.format(days=days, hours=hours, minutes=minutes, seconds=seconds,)
 
     return ""
-
-
-def write_lines(lines: Iterable[str], path: Path):
-    content = "\n".join(lines) + "\n"
-    path.write_text(content)
 
 
 class StatusWatcher(Extension):
@@ -114,3 +106,22 @@ class StatusWatcher(Extension):
 
         except Exception as e:
             Logger.log("w", "failed to write status file: {}".format(e))
+
+
+class StatusTest(unittest.TestCase):
+    def test_duration_text(self):
+        pairs = [
+            (45, "0:00:45"),
+            (145, "0:02:25"),
+            (3145, "0:52:25"),
+            (3845, "1:04:05"),
+            (7845, "2:10:45"),
+        ]
+
+        for value, expected in pairs:
+            with self.subTest((value, expected)):
+                self.assertEqual(duration_text(value), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
